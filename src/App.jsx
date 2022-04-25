5f644a40e6412ff999864aba50a5654ec7e89551
andrey678 <drew6496@yandex.ru>
2022-04-25 21:19:59 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Calendar from './Calendar';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Calendar />
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/Calendar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './index.css';
+
+class Calendar extends React.Component{
+    
+}
+
+export default Calendar;
