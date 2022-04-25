be4a174e7bb689b5b2916f48f3256fb8bec40221
andrey678 <drew6496@yandex.ru>
2022-04-25 23:11:01 +0300

    Add interaction to buttons and selects. Add ability to choose date. Use 'ref' attribute to get value of selects and update the state.

--- src/Calendar/index.jsx
+++ src/Calendar/index.jsx
@@ -4,9 +4,56 @@ import './index.css';
 
 class Calendar extends React.Component{
     static defaultProps = {
+        date: new Date(),
         years: [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026,2027,2028,2029,2030],
         monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-        weekDayNames: ['Пн','Вт','Ср','Чт','Пт','Сб','Вс']
+        weekDayNames: ['Пн','Вт','Ср','Чт','Пт','Сб','Вс'],
+        onChange: Function.prototype
+    };
+    // Состояние для хранения выбора месяца и года, текущей даты и выбранной даты
+    state = {
+        date: this.props.date,
+        currentDate: new Date(),
+        selectedDate: null
+    };
+
+    get year(){
+        return this.state.date.getFullYear();
+    }
+    get month(){
+        return this.state.date.getMonth();
+    }
+    get day(){
+        return this.state.date.getDate();
+    }
+    // Обработчик кнопки назад для месяца
+    handlePrevMonthButtonClick = () => {
+        const date = new Date(this.year,this.month - 1);
+
+        this.setState({date});
+    };
+    // Обработчик кнопки вперёд для месяца
+    handleNextMonthButtonClick = () => {
+        const date = new Date(this.year,this.month + 1);
+
+        this.setState({date});
+    };
+    // Метод для обработки календаря
+    handleSelectChange = () => {
+        // Значение года из селекта года
+        const year = this.yearSelect.value;
+        // Значение месяца из селекта месяца
+        const month = this.monthSelect.value;
+        // Создаём новую дату
+        const date = new Date(year, month);
+        // Обновляем состояние
+        this.setState({date});
+    };
+    // Обработчие при нажатии на день в календаре
+    handleDayClick = date => {
+        this.setState({selectedDate: date});
+        // Сообщаем родительскому компоненту, что была выбрана новая дата
+        this.props.onChange(date);
     };
     render(){
         const { years, monthNames, weekDayNames } = this.props;
@@ -21,18 +68,26 @@ class Calendar extends React.Component{
             <div className="calendar">
                 {/* Элементы управления */}
                 <header>
-                    <button>{'<'}</button>
-                    <select>
+                    <button onClick={this.handlePrevMonthButtonClick}>{'<'}</button>
+                    <select
+                        ref={element => this.monthSelect = element}
+                        defaultValue={this.month}
+                        onChange={this.handleSelectChange}
+                    >
                         {monthNames.map((name, index) => 
                             <option key={name} value={index}>{name}</option>
                         )}
                     </select>
-                    <select>
+                    <select 
+                        ref={element => this.monthSelect = element}
+                        defaultValue={this.year}    
+                        onChange={this.handleSelectChange}
+                    >
                         {years.map(year => 
                             <option key={year} value={year}>{year}</option>    
                         )}
                     </select>
-                    <button>{'>'}</button>
+                    <button onClick={this.handleNextMonthButtonClick}>{'>'}</button>
                 </header>
                 {/* Календарь */}
                 <table>
@@ -49,7 +104,11 @@ class Calendar extends React.Component{
                         {monthData.map((week, index) =>
                             <tr key={index} className="week">
                                 {week.map((date, index) => date ?
-                                    <td key={index} className="day">{date.getDate()}</td>
+                                    <td 
+                                        key={index} 
+                                        className="day"
+                                        onClick={() => this.handleDayClick(date)}
+                                        >{date.getDate()}</td>
                                     :
                                     <td key={index} />
                                 )}
